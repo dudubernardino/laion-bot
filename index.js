29b0365713f77cdf342ba050dfe58a4546588ebb
Eduardo Bernardino <eduardomoura.moura@mail.com>
2020-02-27 13:19:27 -0300

    First Commit

---
+++ index.js
@@ -0,0 +1,49 @@
+require("dotenv").config();
+const Twit = require("twit");
+
+const Bot = new Twit({
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET,
+  access_token: process.env.ACCESS_TOKEN,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+  timeout_ms: 60 * 1000
+});
+
+console.log("Este bot está rodando...");
+
+const BotInit = () => {
+  let query = {
+    q: "Laion",
+    result_type: "recent"
+  };
+
+  const BotGotLatestTweet = (err, data, response) => {
+    if (err) {
+      console.log("Bot could not find latest tweet, : " + err);
+    } else {
+      data.statuses.map(tweet => {
+        const id = {
+          id: tweet.id_str
+        };
+
+        try {
+          Bot.post("statuses/retweet/:id", id, (err, response) => {
+            if (err) {
+              console.log("Bot could not retweet, : " + err);
+            } else {
+              console.log("Bot retweeted : " + id.id);
+            }
+          });
+        } catch (err) {
+          console.log(err);
+        }
+      });
+    }
+  };
+
+  Bot.get("search/tweets", query, BotGotLatestTweet);
+};
+
+setInterval(() => {
+  BotInit();
+}, 10 * 60 * 1000);
